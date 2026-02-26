3ca222d2d587d3ebb363ece42ce8ad488ae0e8c7
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-02-26 15:01:59 +1300

    proper adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,23 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
-  kit: {
-    // Cloudflare adapter (used only when building for production)
-    adapter: adapter(),
-
-    // You can leave the default paths for local dev
-    // out: '.svelte-kit/output' // optional
-  }
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			config: undefined,
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			},
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
 
 export default config;
