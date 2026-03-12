4611634a558c848f24a2a6a126eec09e9f4b2548
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-03-13 09:25:46 +1300

    proper favicon routing, page titles, and landing screen for mobile users

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -1,2 +1,6 @@
+<svelte:head>
+    <title>Contribute to the project</title>
+</svelte:head>
+
 <h1>Contribute</h1>
 <h3>Information on contributing coming soon!</h3>
--- src/routes/takedown/+page.svelte
+++ src/routes/takedown/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <title>Takedown requests</title>
+</svelte:head>
+
 <h1>Takedown requests</h1>
 
 <p>If you would like to request that material in our archives be redacted or removed (for example, due to copyright or sensitive content), please <a href="/contact">contact us</a>. Requests will be forwarded to the archive administrators for review.</p>
