19ebcc841a106817cd0a830f6aa17544505232fa
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-03-12 14:46:25 +1300

    assorted  UI/UX and markup changes

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -1,1 +1,2 @@
 <h1>Contribute</h1>
+<h3>Information on contributing coming soon!</h3>
--- src/routes/privacy/+page.svelte
+++
@@ -1,5 +0,0 @@
-<h1>Privacy policy and takedown requests</h1>
-<h2>Privacy</h2>
-
-<h2>Takedown requests</h2>
-<p>If you wish to have something in our archives redacted, please <a href="/contact">contact us</a>.</p>
